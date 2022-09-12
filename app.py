39305ce109572bcdfdbb963f8a3162311431ffdb
Shubh Verma <shubh.verma.19cs@bmu.edu.in>
2022-09-12 10:01:32 +0530

    flask and paho clients

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_mqtt import Mqtt
+import flask_mqtt
+
+app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = '0.0.0.0'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = 'user1'
+app.config['MQTT_PASSWORD'] = 'pass1'
+app.config['MQTT_REFRESH_TIME'] = 5.0
+
+mqtt = Mqtt(app)
+
+# Debug setting set to true
+app.debug = True
+
+
+# @app.route('/')
+# def index():
+#     return "Hello World on Flask"
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('home/mytopic')
+    mqtt.publish('home/mytopic','hello world',1,True)
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    if level == flask_mqtt.MQTT_LOG_ERR:
+        print('Error: {}'.format(buf))
+
+if __name__ == '__main__':
+    app.run()
---
+++ paho_client.py
@@ -0,0 +1,35 @@
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    # client.subscribe("$SYS/#")
+    client.subscribe("mytopic", 2)
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    publish.single("mytopic", "Hello World", 1, retain=True, hostname="0.0.0.0", auth = {'username':"user1", 'password':"pass1"})
+
+
+client = mqtt.Client()
+
+client.username_pw_set(username="user1", password="pass1")
+client.on_connect = on_connect
+# client.on_subscribe = on_subscribe
+client.on_message = on_message
+
+client.connect("0.0.0.0", 1883, keepalive=600)
+publish.single("mytopic", "Hello World", 1, retain=True, hostname="0.0.0.0", auth = {'username':"user1", 'password':"pass1"})
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
